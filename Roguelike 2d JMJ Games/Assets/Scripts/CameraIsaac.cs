724b30052ad63a12472e8cd54a68fcc139c28740
Josep <josep.mora..7e3@itb.cat>
2020-12-02 13:34:54 +0100

    principio camara

---
+++ Roguelike 2d JMJ Games/Assets/Scripts/CameraIsaac.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraIsaac : MonoBehaviour
+{
+    public GameObject CurrentRoom;
+    private Vector3 ref1;
+
+    public float smoothness;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.position!=new Vector3(CurrentRoom.transform.x,CurrentRoom.transform.y,-10)){
+           transform.position = Vector3.SmoothDamp(transform.position, CurrentRoom.transform.position, ref ref1, smoothness); 
+        }
+    }
+}
