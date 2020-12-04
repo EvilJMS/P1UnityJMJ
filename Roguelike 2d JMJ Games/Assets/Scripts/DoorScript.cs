f5b454d599906c26e4439e75bfe45e79e20c6278
Josep <josep.mora..7e3@itb.cat>
2020-12-04 13:37:35 +0100

    Puerta script finalizado

---
+++ Roguelike 2d JMJ Games/Assets/Scripts/DoorScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorScript : MonoBehaviour
+{
+    public Sprite Locked, Unlocked;
+    public bool locked;
+    public BoxCollider2D collider;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (locked==true)
+        {
+            GetComponent<SpriteRenderer>().sprite = Locked;
+            collider.enabled = true;
+        }
+        else if (locked==false)
+        {
+            GetComponent<SpriteRenderer>().sprite = Unlocked;
+            collider.enabled = false;
+        }
+        
+    }
+}
