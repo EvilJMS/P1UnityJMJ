31de828fabe465c2fe79119d46895e48afe641c3
Javier <javier.romero.7e3@itb.cat>
2020-12-06 21:50:03 +0100

    Barra de corazones

---
+++ Roguelike 2d JMJ Games/Assets/Scripts/HealthSystem.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthSystem : MonoBehaviour
+{
+    public int vida;
+    public int numCorazones;
+
+    public Image[] corazones;
+    public Sprite corazonCompleto;
+    public Sprite corazonVacio;
+
+    void Update()
+    {
+        if (vida > numCorazones)
+        {
+            vida = numCorazones;
+        }
+
+        for (int i = 0; i < corazones.Length; i++)
+        {
+            if (i<vida)
+            {
+                corazones[i].sprite = corazonCompleto;
+            }
+            else
+            {
+                corazones[i].sprite = corazonVacio;
+            }
+
+            if (i<numCorazones)
+            {
+                corazones[i].enabled = true;
+            }
+            else
+            {
+                corazones[i].enabled = false;
+            }
+        }
+    }
+}
