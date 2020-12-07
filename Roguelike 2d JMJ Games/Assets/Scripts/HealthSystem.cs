6bc532b7073a3e878d7a37f0919abd77289a25d6
Josep <josep.mora.7e3@itb.cat>
2020-12-07 17:32:54 +0100

    Añadidos scripts de enemigos

---
+++ Roguelike 2d JMJ Games/Assets/Scripts/HealthSystem.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthSystem : MonoBehaviour
+{
+    public int vida;
+    public int numCorazones;
+
+    public Image[] corazones;
+    public Sprite corazonCompleto;
+    public Sprite corazonVacio;
+
+    private void Start()
+    {
+        DibujarCorazones();
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void TakeDamage(int damage)
+    {
+        vida -= damage;
+        if (vida < 0)
+        {
+            vida = 0;
+        }
+        DibujarCorazones();
+    }
+
+    void DibujarCorazones()
+    {
+        if (vida > numCorazones)
+        {
+            vida = numCorazones;
+        }
+
+        for (int i = 0; i < corazones.Length; i++)
+        {
+            if (i < vida)
+            {
+                corazones[i].sprite = corazonCompleto;
+            }
+            else
+            {
+                corazones[i].sprite = corazonVacio;
+            }
+
+            if (i < numCorazones)
+            {
+                corazones[i].enabled = true;
+            }
+            else
+            {
+                corazones[i].enabled = false;
+            }
+        }
+    }
+
+    
+}
