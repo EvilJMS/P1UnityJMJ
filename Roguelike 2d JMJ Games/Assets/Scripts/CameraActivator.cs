6e0142f19967b61de467c4d90381003ea2c878dd
Josep <josep.mora..7e3@itb.cat>
2020-12-09 19:09:03 +0100

    Camera activator 1.0

--- Roguelike 2d JMJ Games/Assets/Scripts/CameraActivator.cs
+++ Roguelike 2d JMJ Games/Assets/Scripts/CameraActivator.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class CameraActivator : MonoBehaviour
 {
+    public GameObject[] camerasActivator;
+    bool on;
     // Start is called before the first frame update
     void Start()
     {
