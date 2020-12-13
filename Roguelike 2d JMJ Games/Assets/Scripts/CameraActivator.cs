bb46e4ffe2e058bb8255ef8764903c833049f0bd
Josep <josep.mora.7e3@itb.cat>
2020-12-13 22:28:39 +0100

    Todas las salas con tiles y camara finalizada
    Falta testear las camaras

--- Roguelike 2d JMJ Games/Assets/Scripts/CameraActivator.cs
+++ Roguelike 2d JMJ Games/Assets/Scripts/CameraActivator.cs
@@ -5,26 +5,23 @@ using UnityEngine;
 public class CameraActivator : MonoBehaviour
 {
     public GameObject[] cameras;
-    bool on;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     void OnTriggerEnter2D(Collider2D other){
-        on = cameras[0].GetComponent<MoveCamera>().on;
-        if (on == false)
+        foreach (GameObject camera in cameras)
         {
-            foreach (GameObject camera in cameras)
-            {
-                camera.GetComponent<MoveCamera>().on = true;
+            if (camera.GetComponent<MoveCamera>().on == false) {
+              camera.GetComponent<MoveCamera>().on = true;
             }
         }
     }
