0f3c05d4d9863a90b71bcceea41661fce6ebba6f
Javier <javier.romero.7e3@itb.cat>
2020-12-14 18:01:29 +0100

    Daño balas y vida enemigos

--- Roguelike 2d JMJ Games/Assets/Scripts/BulletScript.cs
+++ Roguelike 2d JMJ Games/Assets/Scripts/BulletScript.cs
@@ -4,20 +4,27 @@ using UnityEngine;
 
 public class BulletScript : MonoBehaviour
 {
-  public float lifetime;
+    public float lifetime;
+  
+    public int damage;
+
     // Start is called before the first frame update
     void Start()
     {
         StartCoroutine(DeathDelay());
     }
 
-    // Update is called once per frame
-    void Update()
-    {
 
-    }
     IEnumerator DeathDelay(){
       yield return new WaitForSeconds(lifetime);
       Destroy(gameObject);
+    }
+
+    
+    void OnCollisionEnter2D(collision){
+      if (collision.gameObject.CompareTag("Enemy"))
+      {
+          collision.gameObject.getComponent<EnemyHealth>().LowHP(damage);
+      }
     }
 }
---
+++ Roguelike 2d JMJ Games/Assets/Scripts/EnemyHealth.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+    public float maxHealth;
+
+    public int damage;
+
+    private float currentHealth;
+    
+    void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+    
+
+    void LowHP(int damage){
+        currentHealth -= damage;
+        if (currentHealth < 0)
+        {
+            currentHealth = 0;
+        }
+    }
+
+}
