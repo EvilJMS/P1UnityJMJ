0a0011ffb50096a2d718d167c5568c9e822fc444
Javier <javier.romero.7e3@itb.cat>
2020-12-16 18:45:32 +0100

    Solucionar problemas con balas y vida enemigos

---
+++ Roguelike 2d JMJ Games/Assets/Scripts/EnemyHealth.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+    public float maxHealth;
+
+    public float currentHealth;
+    
+    void Start()
+    {
+        currentHealth = maxHealth;
+    }
+    
+
+    public void LowHP(int damage){
+        currentHealth -= damage;
+        if (currentHealth < 0)
+        {
+            currentHealth = 0;
+        }
+    }
+}
