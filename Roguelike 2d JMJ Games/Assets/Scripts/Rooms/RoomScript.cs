da7bd18d966e35c142f70c6fb4beec81bcf1a513
Josep <josep.mora..7e3@itb.cat>
2020-12-16 18:51:50 +0100

    Creado scena de tutorial y script puertas 80%

--- Roguelike 2d JMJ Games/Assets/Scripts/Rooms/RoomScript.cs
+++ Roguelike 2d JMJ Games/Assets/Scripts/Rooms/RoomScript.cs
@@ -7,6 +7,10 @@ public class RoomScript : MonoBehaviour
     public GameObject[] Doors;
     public bool Open;
     public bool openDoors;
+    public bool firstTime;
+    public bool allDead;
+
+    public GameObject[] enemies;
     
     // Start is called before the first frame update
     void Start()
@@ -32,6 +36,33 @@ public class RoomScript : MonoBehaviour
             {
                 Door.GetComponent<DoorScript>().locked = false;
             } 
+        }
+
+        for (int i = 0; i < enemies.Length; i++)
+        {
+            if (enemies[i]!=null)
+            {
+                allDead=false;
+            } else
+            {
+                allDead=true;
+            }
+        }
+        if (allDead)
+        {
+            Open = true;
+        }
+    }
+
+    
+    private void OnTriggerEnter2D(Collider2D other){
+        if (other.CompareTag("Player"))
+        {
+            if (firstTime == false)
+            {
+                firstTime = true;
+                Open = false;
+            }
         }
     }
 
