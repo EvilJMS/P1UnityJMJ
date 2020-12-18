50070268800f365bd2a9127351a7bc760281a041
Josep <josep.mora..7e3@itb.cat>
2020-12-18 18:20:01 +0100

    puerta arreglada

--- Roguelike 2d JMJ Games/Assets/Scripts/Rooms/RoomScript.cs
+++ Roguelike 2d JMJ Games/Assets/Scripts/Rooms/RoomScript.cs
@@ -4,13 +4,14 @@ using UnityEngine;
 
 public class RoomScript : MonoBehaviour
 {
+    public GameObject[] Enemies;
     public GameObject[] Doors;
     public bool Open;
     public bool openDoors;
     public bool firstTime;
     public bool allDead;
 
-    public GameObject[] enemies;
+    
     
     // Start is called before the first frame update
     void Start()
@@ -21,16 +22,16 @@ public class RoomScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        openDoors = Doors[0].GetComponent<DoorScript>().locked;
+        
 
-        if (Open==false&&openDoors==true)
+        if (Open==false)
         {
             foreach (GameObject Door in Doors)
             {
                 Door.GetComponent<DoorScript>().locked = true;
             }
         }
-        else if (Open==true&&openDoors==false)
+        else if (Open==true)
         {
            foreach (GameObject Door in Doors)
             {
@@ -38,11 +39,12 @@ public class RoomScript : MonoBehaviour
             } 
         }
 
-        for (int i = 0; i < enemies.Length; i++)
+        for (int i = 0; i < Enemies.Length; i++)
         {
-            if (enemies[i]!=null)
+            if (Enemies[i]!=null)
             {
                 allDead=false;
+            
             } else
             {
                 allDead=true;
