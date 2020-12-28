7851751b41111a7bd9ad4dcce96aecdd090928bb
Josep <josep.mora.7e3@itb.cat>
2020-12-28 21:05:42 +0100

    Creado y probado el spawn de enemigos

--- Roguelike 2d JMJ Games/Assets/Scripts/EnemySpawner.cs
+++ Roguelike 2d JMJ Games/Assets/Scripts/EnemySpawner.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour
 {
+  public bool enemySpawn;
+  public GameObject enemy;
+  GameObject Room;
     // Start is called before the first frame update
     void Start()
     {
-        
+      Room = transform.parent.gameObject;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+    void OnTriggerEnter2D(Collider2D other){
+      if (other.CompareTag("Player")) {
+        if (enemySpawn == false) {
+          GameObject newObject = Instantiate(enemy, transform.position, Quaternion.identity) as GameObject;
+          newObject.GetComponent<EnemyHealth>().currentHealth = newObject.GetComponent<EnemyHealth>().maxHealth;
+          newObject.transform.parent = Room.transform;
+          Room.GetComponent<RoomScript>().addEnemy();
+          enemySpawn=true;
+        }
+      }
     }
 }
