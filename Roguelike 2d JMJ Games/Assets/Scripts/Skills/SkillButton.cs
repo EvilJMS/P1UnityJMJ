b11fb6e82bf9c8a88285820aa14b2b2648cf4ff1
Josep <josep.mora.7e3@itb.cat>
2021-01-02 18:00:38 +0100

    Script para mostrar la info de cada skill al dar click

---
+++ Roguelike 2d JMJ Games/Assets/Scripts/Skills/SkillButton.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SkillButton : MonoBehaviour
+{
+    public Image skillImage;
+    public Text skillNameText;
+    public Text skillDesText;
+    public Text skillCostText;
+
+    public int skillButtonID;
+
+    public void PressSkillButton(){
+      skillImage.sprite = SkillManager.instance.skills[skillButtonID].skillSprite;
+    }
+}
