4562175c1010153929b16f50938e10f7bd5058ef
Josep <josep.mora.7e3@itb.cat>
2021-01-07 23:59:09 +0100

    creada escena de muerte

---
+++ Roguelike 2d JMJ Games/Assets/Scripts/DeathMenu.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DeathMenu : MonoBehaviour
+{
+    public void Retry(){
+      SceneManager.LoadScene(1);
+    }
+
+    public void GoToHub(){
+      SceneManager.LoadScene(3);
+    }
+}
