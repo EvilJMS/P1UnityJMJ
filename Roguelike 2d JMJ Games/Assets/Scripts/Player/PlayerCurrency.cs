3866c3e438c06e34d40c424295dfc181462c4225
Josep <josep.mora.7e3@itb.cat>
2021-01-08 15:11:06 +0100

    Añadido dinero en el canvas

--- Roguelike 2d JMJ Games/Assets/Scripts/Player/PlayerCurrency.cs
+++ Roguelike 2d JMJ Games/Assets/Scripts/Player/PlayerCurrency.cs
@@ -1,24 +1,37 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerCurrency : MonoBehaviour
 {
 
     public int currentCurrency;
+    public Text currencyText;
 
     void Start()
     {
-        
+
         currentCurrency = GlobalControl.Instance.currentCurrency;
+        DisplayMoney();
     }
 
     public void EarnMoney(int money)
     {
         currentCurrency += money;
+        DisplayMoney();
+
+    }
+
+    void Update(){
+      DisplayMoney();
     }
 
     public void SaveData(){
       GlobalControl.Instance.currentCurrency = currentCurrency;
+    }
+
+    private void DisplayMoney(){
+      currencyText.text = currentCurrency + "";
     }
 }
