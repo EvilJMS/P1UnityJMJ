9934d564994b36e5929267d5b78a5f45a526b6b4
Javier <javier.romero.7e3@itb.cat>
2021-01-20 21:51:46 +0100

    Añadido enemigo que dispara y script disparar

---
+++ Roguelike 2d JMJ Games/Assets/Scripts/Enemies/EnemyDisparar.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyDisparar : MonoBehaviour
+{
+    private float timeBtwShots;
+    public float startTimeBtwShots;
+
+    public GameObject proyectil;
+    private Transform player;
+    
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        timeBtwShots = startTimeBtwShots;    
+    }
+
+    
+    void Update()
+    {
+        if (timeBtwShots <= 0)
+        {
+            Instantiate(proyectil, transform.position, Quaternion.identity);
+            timeBtwShots = startTimeBtwShots;
+        }else{
+            timeBtwShots -= Time.deltaTime;
+        }    
+    }
+}
