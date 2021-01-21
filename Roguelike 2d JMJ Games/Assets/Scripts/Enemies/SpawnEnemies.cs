88d8ac4d7d1a7aff08c2ea3b74774cceb6afe3d7
Josep <josep.mora.7e3@itb.cat>
2021-01-21 11:43:05 +0100

    Enemigos metidos en spawn random

--- Roguelike 2d JMJ Games/Assets/Scripts/Enemies/SpawnEnemies.cs
+++ Roguelike 2d JMJ Games/Assets/Scripts/Enemies/SpawnEnemies.cs
@@ -11,7 +11,7 @@ public class SpawnEnemies : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-      rand = Random.Range(0,3);
+      rand = Random.Range(0,4);
     }
 
     // Update is called once per frame
